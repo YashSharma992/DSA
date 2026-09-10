8603d62c17a6236efa183a15b50d009bb915735f
YashSharma992 <syash6183@gmail.com>
2026-09-10 18:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (53.45%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long comma = 0;
-        long long base = 1000;
-        if (n < 1000)
-            return 0;
-        else {
-            while (base <= n) {
-                comma += (n - base + 1);
-                base *= 1000;
-            }
+        long long comma=0;
+        long long base=1000;
+        while(base<=n){
+            comma+=(n-base+1);
+            base*=1000;
         }
-
         return comma;
+
+
+        // long long comma = 0;
+        // long long base = 1000;
+        // if (n < 1000)
+        //     return 0;
+        // else {
+        //     while (base <= n) {
+        //         comma += (n - base + 1);
+        //         base *= 1000;
+        //     }
+        // }
+
+        // return comma;
 
         //     long long temp=0;
 
