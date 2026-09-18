ee413c07e89b48333752b0172a33db4e477ca6bc
YashSharma992 <syash6183@gmail.com>
2026-09-18 19:36:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.35%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,28 +1,51 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>ans(m+n);
-        int i=0,j=0;
-        while(i<m&&j<n){
+        int k=n+m-1;
+        int j=n-1;
+        int i=m-1;
+        while(i>=0 && j>=0){
             if(nums1[i]<=nums2[j]){
-                ans[i+j]=nums1[i];
-                i++;
+                nums1[k]=nums2[j];
+                k--;
+                j--;
             }
             else{
-                ans[i+j]=nums2[j];
-                j++;
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
         }
-        while(i<m){
-            ans[i+j]=nums1[i];
-            i++;
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
         }
-        while(j<n){
-            ans[i+j]=nums2[j];
-            j++;
-        }
-        for(int i=0;i<n+m;i++){
-            nums1[i]=ans[i];
-        }
+
+        
+        // vector<int>ans(m+n);
+        // int i=0,j=0;
+        // while(i<m&&j<n){
+        //     if(nums1[i]<=nums2[j]){
+        //         ans[i+j]=nums1[i];
+        //         i++;
+        //     }
+        //     else{
+        //         ans[i+j]=nums2[j];
+        //         j++;
+        //     }
+        // }
+        // while(i<m){
+        //     ans[i+j]=nums1[i];
+        //     i++;
+        // }
+        // while(j<n){
+        //     ans[i+j]=nums2[j];
+        //     j++;
+        // }
+        // for(int i=0;i<n+m;i++){
+        //     nums1[i]=ans[i];
+        // }
     }
 };
