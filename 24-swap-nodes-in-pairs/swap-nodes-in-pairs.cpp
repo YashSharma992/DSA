40410ca4c9181d38203931d9196a0957a98b2522
YashSharma992 <syash6183@gmail.com>
2026-09-25 17:17:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.86%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,24 +11,40 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-       if(!head)
-       return head;
-       ListNode*temp=head;
-       for(int i=0;i<2;i++){
-        if(!temp)
-        return head;
-        temp=temp->next;
-       }
-       ListNode*curr=head;
-       ListNode*prev=NULL;
-       for(int i=0;i<2;i++){
-        ListNode*nex=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=nex;
-       }
-       head->next = swapPairs(curr);
-       return prev;
+        ListNode* dummy= new ListNode(0,head);
+        ListNode* prev=dummy;
+        while(prev->next && prev->next->next){
+            ListNode* first=prev->next;
+            ListNode* sec=prev->next->next;
+
+            prev->next=sec;
+            first->next=sec->next;
+            sec->next=first;
+
+            prev=first;
+        }
+        return dummy->next;
+
+
+
+    //    if(!head)
+    //    return head;
+    //    ListNode*temp=head;
+    //    for(int i=0;i<2;i++){
+    //     if(!temp)
+    //     return head;
+    //     temp=temp->next;
+    //    }
+    //    ListNode*curr=head;
+    //    ListNode*prev=NULL;
+    //    for(int i=0;i<2;i++){
+    //     ListNode*nex=curr->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=nex;
+    //    }
+    //    head->next = swapPairs(curr);
+    //    return prev;
 
         // vector<int>ans;
         // ListNode*temp=head;
